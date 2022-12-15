fbf31bd059b5ec84730353f671047f222ed6d4bf
sabrinalupsan <sabrinalupsan@gmail.com>
2022-12-15 14:29:06 +0200

    new

--- Mathematics/src/maths/MathematicsTestCaseProduct.java
+++ Mathematics/src/maths/MathematicsTestCaseProduct.java
@@ -1,21 +1,19 @@
 package maths;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
-class MathematicsTestCaseSum {
+
+public class MathematicsTestCaseProduct {
 	
 	Mathematics m;
 
 	@Test
-	void testSum() {
+	void testProduct() {
 		m.a = 3;
 		m.b = 4;
-		int result = m.sum(m.a,  m.b);
-		assertEquals(result, 7);
+		int result = m.product(m.a,  m.b);
+		assertEquals(result, 12);
 	}
-	
-	
 
 }
