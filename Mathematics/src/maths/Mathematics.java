2c7b4b015a878ea40210984e9d379979e88a0271
sabrinalupsan <sabrinalupsan@gmail.com>
2022-12-15 14:30:35 +0200

    new

--- Mathematics/src/maths/Mathematics.java
+++ Mathematics/src/maths/Mathematics.java
@@ -1,2 +1,19 @@
 package maths;
 
+class Mathematics {
+	
+	public static int a, b;
+	public static String choice;
+	
+	public static int sum(int a, int b) {
+		return a+b;
+	}
+	
+	public static int product(int a, int b) {
+		return a*b;
+	}
+
+	public static void main(String[] args) {
+	} 
+
+}
